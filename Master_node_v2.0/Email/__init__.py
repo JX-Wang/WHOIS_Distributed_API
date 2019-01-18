e6531e7c4af2bc92de72be89e569b0918af65dfe
WJX.WUD <wjx.wud@gmail.com>
2019-01-18 15:10:17 +0800

    delete all

--- Master_node_v2.0/Email/__init__.py
+++
@@ -1,9 +0,0 @@
-#! usr/bin/env python
-# coding:utf-8
-
-"""
-email模块
-监测API子点的状态
-"""
-
-__date = "2018.8.17"
--- Master_node_v2.0/whois_connect/__init__.py
+++
@@ -1,8 +0,0 @@
-#! usr/bin/env python
-# coding:utf-8
-
-"""
-whois connect model
-"""
-
-__date = "2018.8.30"
